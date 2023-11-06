4dc303667e098c6d4de3a09f31220d63d596d05c
Idedav <xdavide.correa@gmail.com>
2023-11-06 15:42:11 +0100

    [add] class Movie [add] function setImage

--- index.php
+++ index.php
@@ -1,22 +1,40 @@
 <?php 
 
+    class Media{
+        public $file_name;
+        public $name;
+        public $type = "image";
+
+        public function __construct($_file_name, $_name){
+            $this->file_name = $_file_name;
+            $this->name = $_name;
+        }
+    }
+
     class Movie {
         public $name;
         public $original_name;
         public $gen;
         public $duration;
-        public function __construct( string $_name, string $_original_name, array $_gen, int $_duration){
+        public $image;
+
+        public function setImage(Media $_image){
+            $this->image = $_image;
+        }
+
+        public function __construct( string $_name, string $_original_name, array $_gen, int $_duration, Media $_image = null){
             $this->name = $_name;
             $this->original_name = $_original_name;
             $this->gen = $_gen;
             $this->duration = $_duration;
+            $this->image = $_image;
         }
     }
 
-    $maze_runner = new Movie('Il Labirinto', 'Maze Runner', ['Azione'], 120 );
+    $movies = [
+        new Movie('Il Labirinto', 'Maze Runner', ['Azione'], 120, new Media('maze_runner.jpg', 'Maze Runner')),
+        new Movie('Barbie', 'Barbie', ['Commedia'], 90, new Media('barbie.jpg', 'Barbie'))
+    ];
 
-    $barbie = new Movie('Barbie', 'Barbie', ['Commedia'], 90);
-
-    var_dump($maze_runner);
-    var_dump($barbie);
+    var_dump($movies);
 ?>
